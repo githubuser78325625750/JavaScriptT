363bec186e96bd4610fdae0993120c1d0a83b420
Thiago Dos Santos <110904470+githubuser78325625750@users.noreply.github.com>
2023-09-15 08:30:28 -0300

    JaVA2
    um siTe em Java

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function tocaSomPom () {
 
-    document.querySelector('#som_tecla_pom').play()
+    document.querySelector('#som_tecla_pom').play();
 
 
 }
 
 
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    
 
-
+const listaDeTeclas = document.querySelectorAlls('.tecla');
 
