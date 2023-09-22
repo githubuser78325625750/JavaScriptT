dc3dc2e36d5750e97b72515bb46388054fce1e77
Thiago Dos Santos <110904470+githubuser78325625750@users.noreply.github.com>
2023-09-22 08:46:23 -0300

    .
    .

--- main.js
+++ main.js
@@ -12,13 +12,26 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
   
 let contador = 0;
 
-while (contador < listaDeTeclas.length){
+while (contador < listaDeTeclas.length) {
 
-listaDeTeclas [contador].onclick = tocaSom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+ 
+    const idAudio = `#som_${instrumento}`;
 
-contador = contador + 1;
+    tecla.onclick = function () {
 
-console.log(contador);
+tocaSom ('idAudio');
 
 }
+
+contador = contador + 1;
+console.log(contador):
+
+    }
+
+
+
+
+
 
