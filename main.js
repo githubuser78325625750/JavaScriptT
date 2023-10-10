7db98af4c85a545118060c14929ccde36498aad2
Thiago Dos Santos <110904470+githubuser78325625750@users.noreply.github.com>
2023-10-10 07:46:47 -0300

    Atualizar o main.js
    teste com For (Para)

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
   
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+ For (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -25,8 +25,8 @@ tocaSom (idAudio);
 
 }
 
-contador = contador + 1;
-console.log(contador);
+contador = contador++;
+
     }
 
 
